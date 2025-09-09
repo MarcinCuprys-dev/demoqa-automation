917d937c98599aa8c1b8986053f4eab9b6daab82
Marcin Cuprys <marcin.cuprys.qa@gmail.com>
2025-09-09 13:19:38 +0200

    add link response status verification

---
+++ pages/links/selectors.page.ts
@@ -0,0 +1,10 @@
+export const linksSelectors = {
+  createdLink: "a#created",
+  noContentLink: "a#no-content",
+  movedLink: "a#moved",
+  badRequestLink: "a#bad-request",
+  unauthorizedLink: "a#unauthorized",
+  forbiddenLink: "a#forbidden",
+  notFoundLink: "a#invalid-url",
+  linkResponse: "#linkResponse",
+};
